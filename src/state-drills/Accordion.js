a20179a32c09be5bc53fe9e964e1be836d3dcb98
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-07-06 09:47:09 -0500

    added predid code

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-class Accordion extends React.Component {
-     result = ''
-    // this.props.forEach(element => {
-    //     let temp=(<li>
-    //               <button>
-    //                   {element.title}
-    //               </button>
-    //           </li>)
-    //       console.log(temp);
-    //     result += temp
-    //   })
-    handleClick() {
-        console.log('click')
-    }
-    render(){
-        return(
-            <div>
-            <button onClick={this.handleClick} >
-                click
-            </button>
 
-            <ul>
-                
-            </ul>
-                <p>{}</p>
-            </div>
-        )
-    }    
+export default class Accordion extends Component {
+  static defaultProps = {
+    sections: [{
+        title: 'Section 1',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      },
+      {
+        title: 'Section 2',
+        content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+      },
+      {
+        title: 'Section 3',
+        content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+      }]
+  };
+
+  state = {
+    activeSectionIndex: null,
+  }
+
+  handleSetActiveSecion = (sectionIndex) => {
+    this.setState({ activeSectionIndex: sectionIndex })
+  }
+
+  renderItem(section, idx, activeSectionIndex) {
+    return (
+      <li className='Accordion__item' key={idx}>
+        <button
+          type='button'
+          onClick={() => this.handleSetActiveSecion(idx)}
+        >
+          {section.title}
+        </button>
+        {(activeSectionIndex === idx) && <p>{section.content}</p>}
+      </li>
+    )
+  }
+
+  render() {
+    const { activeSectionIndex } = this.state
+    const { sections } = this.props
+    return (
+      <ul className='Accordion'>
+        {sections.map((section, idx) =>
+          this.renderItem(section, idx, activeSectionIndex)
+        )}
+      </ul>
+    )
+  }
 }
-
-export default Accordion;
